4ed5584898a6bb6225f840371c40d2f9eee81f45
emthangtrung <danhpham612@gmail.com>
2022-12-01 14:00:01 -0800

    done TwoSum

---
+++ Array & Hashing/TwoSum.js
@@ -0,0 +1,32 @@
+let nums = [11, 15, 2, 7];
+
+
+
+const twoSum = ( nums, target ) => {   //O(n) + O(n) = O(n^2)
+    for (let i = 0; i < nums.length; i ++){ // O(n)
+        for ( let j = i + 1; j < nums.length; j++ ){ // O(n)
+            if (nums[i] + nums[j] === target){
+                return [i, j];
+            }
+        }
+    }
+    return "can't find";
+}
+
+const twoSumMap = ( nums, target) => { // O(n)
+    const map = new Map(); // get element in map = O(1) = >> overall = O(n)
+    for ( let i = 0; i < nums.length; i++){
+        let remain = target - nums[i];
+        let getindexOfRemain = map.get(remain);
+        if (map.has(remain)){
+            return [getindexOfRemain , i];
+        }
+        map.set(nums[i], i)
+    }
+    return "can't find";
+}
+
+
+
+console.log(twoSum(nums, 9));
+console.log(twoSumMap(nums, 9));
