20155b75eb9759a1b90ee1ea3883d0418aad2b11
emthangtrung <danhpham612@gmail.com>
2022-12-02 00:23:09 -0800

    Done ValidAnagram

--- Array & Hashing/ValidAnagram.js
+++ Array & Hashing/ValidAnagram.js
@@ -1,22 +1,48 @@
-// compare 2 string 
+// compare 2 string
 // return true if 2 string is anagram
 // return false if 2 string is not anagram
 // s1 : "anagram"         s2: "nagaram"       => true
 // s1 : "rat"             s2: "car"           => false
 
-function checkAnagram(s1, s2){
-    if( s1.length != s2.length) return false;
+let s1 = "danh";
+let s2 = "hnbd";
 
-    for ( var i = 0; i < s1.length; i++){
+const map = new Map();
 
+const isAnagram = (s1, s2) => {
+  if (s1.length != s2.length) return false; // if dif length false right away
+
+  // get freqency of char in s1
+  for (let i = 0; i < s1.length; i++) {
+    let char = s1.charAt(i);
+
+    if (map.has(char)) {
+      let value = map.get(char);
+      map.set(char, value + 1);
+    } else {
+      map.set(char, 1);
     }
-    return true;
-}
+  }
 
-var a = "danh"
+  for (let i = 0; i < s2.length; i++) {
+    // subtract value from key char in s2
+    let char = s2.charAt(i);
 
-var b = "hand"
+    if (map.has(char)) {
+      let value = map.get(char);
+      map.set(char, value - 1);
+    }
+  }
 
-var c = checkAnagram(a,b);
+  //check if map has all value == 0
+  for (const [char, count] of map) {
+    // O(n)
+    const isEmpty = count === 0;
+    if (!isEmpty) return false;
+  }
+  return true;
+};
+
+let c = isAnagram(s1, s2);
 
 console.log(c);
